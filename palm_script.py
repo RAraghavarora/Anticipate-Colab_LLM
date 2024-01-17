33e57ee2046d8430cef076f00f0a92822533f860
RAraghavarora <raghavarora999@yahoo.in>
2024-01-17 13:26:40 +0530

    Blacken the file

--- palm_script.py
+++ palm_script.py
@@ -6,26 +6,30 @@ from keyconfig import gemini as palm_api
 import pprint
 import google.generativeai as palm
 import random
+
 palm.configure(api_key=palm_api)
 
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+models = [
+    m for m in palm.list_models()
+    if "generateText" in m.supported_generation_methods
+]
 model = models[0].name
 
 import random
 
-f = open('./new_task.json', 'r')
+f = open("./new_task.json", "r")
 task_dict = json.load(f)
 f.close()
 
-f = open('./object_2.json', 'r')
+f = open("./object_2.json", "r")
 objects = json.load(f)
 f.close()
 
-f = open('./receptacle.json', 'r')
+f = open("./receptacle.json", "r")
 receptacles = json.load(f)
 f.close()
 
-f = open('./task.json', 'r')
+f = open("./task.json", "r")
 task_sample_space = json.load(f)
 f.close()
 
@@ -33,12 +37,13 @@ from IPython.display import display
 from IPython.display import Markdown
 import textwrap
 
+
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace("•", "  *")
+    return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
 
 
-inp1 = f'''
+inp1 = f"""
 # The following tasks are possible in the household
 tasks_sample_space = {task_sample_space}
 
@@ -50,9 +55,9 @@ Objects available: {objects}
 
 Today is Wednesday
 User Preference: All the dishes are dirty please clean them.
-'''
+"""
 
-op1 = '''
+op1 = """
 {
     'chain of thought': "Following last Wednesday's routine. Add 'wash the dishes' as the 1st task to the task list, and keeping everything else the same.",
     'tasks' = [
@@ -67,7 +72,8 @@ op1 = '''
         "Prepare medicines"
     ],
 }
-'''
+"""
+
 
 def main():
     results = {}
@@ -85,10 +91,9 @@ def main():
         count = 0
         # if palm_temp == 0.2:
         #     import pdb; pdb.set_trace()
-        while count<500:
+        while count < 500:
             while True:
-                
-                prompt = f'''
+                prompt = f"""
 # The following tasks are possible in the household
 tasks_sample_space = {task_sample_space}
 
@@ -99,8 +104,8 @@ task_last_week = {task_dict}
 Objects available: {objects}
 
 # Input: Today is Thursday. User Preference: I want to eat *seafood* for *dinner*.
-'''
-                model = palm.GenerativeModel('gemini-pro')
+"""
+                model = palm.GenerativeModel("gemini-pro")
                 convo = model.start_chat(history=[
                     {
                         "role": "user",
@@ -118,10 +123,12 @@ Objects available: {objects}
                     #     "role": "model",
                     #     "parts": [op2]
                     # },
-                    ])
+                ])
                 response = convo.send_message(prompt)
                 print(convo.last.text)
-                import pdb; pdb.set_trace()
+                import pdb
+
+                pdb.set_trace()
 
                 completion = palm.generate_text(
                     model=model,
@@ -138,36 +145,50 @@ Objects available: {objects}
                     break
                 else:
                     print("Not working temp = ", palm_temp)
-            import pdb; pdb.set_trace()
+            import pdb
+
+            pdb.set_trace()
             for candidate in completion.candidates[:1]:
-                task_count += len(final_combined_lists_op) + len(final_combined_lists_ip)
-                code_to_execute = candidate['output'].strip()  # Removing triple quotes
+                task_count += len(final_combined_lists_op) + len(
+                    final_combined_lists_ip)
+                code_to_execute = candidate["output"].strip(
+                )  # Removing triple quotes
 
                 env = {}
                 try:
                     exec(code_to_execute, env)
                 except Exception as e:
                     print(e)
-                    print('why bro?')
+                    print("why bro?")
                     continue
-                
+
                 try:
-                    routine_3_output = env['routine_3_output']
+                    routine_3_output = env["routine_3_output"]
                 except:
-                    print('Error in execution?')
+                    print("Error in execution?")
                     try:
                         routine_3_output = eval(code_to_execute)
                     except:
-                        print('Error in execution.')
-                        import pdb; pdb.set_trace()
+                        print("Error in execution.")
+                        import pdb
+
+                        pdb.set_trace()
                         continue
-                import pdb; pdb.set_trace()
-                tau, missing = kendal_tau(routine_3_output, final_combined_lists_op, final_seq)
+                import pdb
+
+                pdb.set_trace()
+                tau, missing = kendal_tau(routine_3_output,
+                                          final_combined_lists_op, final_seq)
                 final_tau.append(tau)
                 final_missing.append(missing)
-                all_sequences = [sequence for sub_sequence in sequences.values() for sequence in sub_sequence]
+                all_sequences = [
+                    sequence for sub_sequence in sequences.values()
+                    for sequence in sub_sequence
+                ]
 
-                temp = [x for x in routine_3_output if x not in set(all_sequences)]
+                temp = [
+                    x for x in routine_3_output if x not in set(all_sequences)
+                ]
 
                 if len(temp) > 0:
                     hallucination_count += 1
@@ -183,24 +204,24 @@ Objects available: {objects}
 
                 print(count)
 
-            
-        results[palm_temp] = {  
-            'Average Ordered count': ordered_count / count,
-            'Average Hallucination count': hallucination_count / count,  
-            'Average Missing count': missing_count / count,  
-            'Average Unordered count': unordered_count / count,  
-            'Average Hallucinations': len(hallucinations) / count,  
-            'Average tau per prompt': np.mean(final_tau),  
-            'Average missing per prompt': np.mean(final_missing),  
-            'Total missing': np.sum(final_missing),  
-            'Average task count': task_count / count  
+        results[palm_temp] = {
+            "Average Ordered count": ordered_count / count,
+            "Average Hallucination count": hallucination_count / count,
+            "Average Missing count": missing_count / count,
+            "Average Unordered count": unordered_count / count,
+            "Average Hallucinations": len(hallucinations) / count,
+            "Average tau per prompt": np.mean(final_tau),
+            "Average missing per prompt": np.mean(final_missing),
+            "Total missing": np.sum(final_missing),
+            "Average task count": task_count / count,
         }
 
-    for temp, result in results.items():  
-        print(f'Temperature: {temp}')  
-        for key, value in result.items():  
-            print(f'{key}: {value}')  
-        print('\n')  
+    for temp, result in results.items():
+        print(f"Temperature: {temp}")
+        for key, value in result.items():
+            print(f"{key}: {value}")
+        print("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
