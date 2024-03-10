f6f2b9180a507c04020a2c583d5b51dd1546dece
Raghav Arora <raghavarora999@yahoo.in>
2024-03-10 19:48:14 +0530

    Dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,13 @@
-((nil . ((conda-env-current-name . default)
-         )))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((flycheck-flake8-maximum-line-length . 200)
+                 (flycheck-checker . python-flake8)
+                 (flycheck-python-pylint-executable . "~/miniconda3/envs/default/bin/pylint")
+                 (flycheck-python-flake8-executable . "~/miniconda3/envs/default/bin/flake8")
+                 (flycheck-python-pycompile-executable . "~/miniconda3/envs/default/bin/python")
+                 (python-shell-interpreter . "~/miniconda3/envs/default/bin/python")
+                 (flycheck-python-ruff-executable . "~/miniconda3/envs/default/bin/ruff")
+                 (flycheck-python-pyright-executable . "~/miniconda3/envs/default/bin/pyright")
+                 (flycheck-python-mypy-executable . "~/miniconda3/envs/default/bin/mypy")
+                 (python-environment . "PYTHONPATH=~/hrc/"))))
