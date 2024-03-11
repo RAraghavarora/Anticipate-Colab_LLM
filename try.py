acd600c6fb1ff72a4d4c2cb604a4ea24102d461d
Raghav Arora <raghavarora999@yahoo.in>
2024-03-11 18:38:22 +0530

    try.py is now used to rename files

--- try.py
+++ try.py
@@ -1,34 +1,19 @@
-import json
-import random
+import os
 
+base_dir = 'llm_cache'
 
-def replace_options(tasks, food_options):
-
-    for task_id, task_description in tasks.items():
-        if "(options =" in task_description:
-            start_index = task_description.find("(options = ") + len("(options = ")
-            end_index = task_description.find(")")
-            options = task_description[start_index:end_index].split(", ")
-            print(options)
-            print("\n\n")
-            for i in range(len(options)):
-                option = options[i]
-                if option in food_options:
-                    task_description = task_description.replace(
-                        option, str(food_options[option])
-                    )
-                    tasks[task_id] = task_description
-
-    return tasks
-
-
-f = open("./json_files/task.json", "r")
-task_sample_space = json.load(f)
-f.close()
-
-f = open("./json_files/food.json", "r")
-food = json.load(f)
-f.close()
-
-task_sample_space = replace_options(task_sample_space, food)
-print(task_sample_space)
+for root, dirs, files in os.walk(base_dir):
+    for file in files:
+        if file == 'gemini_prompt':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gemini_cot_prompt'))
+        elif file == 'gemini_response':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gemini_cot_response'))
+        elif file == 'gpt_prompt':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gpt35_cot_prompt'))
+        elif file == 'gpt_response':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gpt35_cot_response'))
+        elif file == 'gemini_nocot_prompt':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gemini_icl_prompt'))
+        elif file == 'gemini_nocot_response':
+            os.rename(os.path.join(root, file), os.path.join(root, 'gemini_icl_response'))
+print("Done")
